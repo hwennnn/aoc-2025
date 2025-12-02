6a9100f3e193d8c3f560240048741d8c9de8b238
Houman <hwendev@gmail.com>
2025-12-02 22:14:55 +0800

    complete day 2 part 1

--- day1/main.py
+++ day1/main.py
@@ -1,7 +1,6 @@
 class Solver:
     def __init__(self, input_file):
         self.input_file = input_file
-        self.solve()
     
     def parse_instructions(self):
         with open(self.input_file, 'r') as file:
