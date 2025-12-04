08f24cd825049b2ce75c1629281e3dffccd5a764
Houman <hwendev@gmail.com>
2025-12-04 16:17:13 +0800

    update day 4 with toposort

--- day4/main.py
+++ day4/main.py
@@ -1,3 +1,6 @@
+from collections import defaultdict, deque
+
+
 class Solver:
     def __init__(self, input_file):
         self.input_file = input_file
@@ -61,8 +64,51 @@ class Solver:
 
         return ans
 
+    def toposort(self):
+        inputs = self.parse_inputs()
+        grid = [list(line) for line in inputs]
+        graph = defaultdict(set)
+        ans = 0
+
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == '@':
+                    for di in range(-1, 2):
+                        for dj in range(-1, 2):
+                            if 0 <= i + di < len(grid) and 0 <= j + dj < len(grid[i]):
+                                if grid[i + di][j + dj] == '@':
+                                    graph[(i, j)].add((i + di, j + dj))
+                                    graph[(i + di, j + dj)].add((i, j))
+
+        queue = deque()
+        visited = set()
+
+        for i, j in graph.keys():
+            if len(graph[(i, j)]) <= 4:
+                visited.add((i, j))
+                queue.append((i, j))
+
+        while queue:
+            new_queue = deque()
+
+            for _ in range(len(queue)):
+                i, j = queue.popleft()
+                ans += 1
+
+                for ni, nj in graph[(i, j)]:
+                    if (ni, nj) not in visited:
+                        graph[(ni, nj)].discard((i, j))
+                        if len(graph[(ni, nj)]) <= 4:
+                            visited.add((ni, nj))
+                            new_queue.append((ni, nj))
+
+            queue = new_queue
+
+        return ans
+
 
 if __name__ == "__main__":
     solver = Solver("input2.txt")
     print(f"Answer 1: {solver.solve1()}")
     print(f"Answer 2: {solver.solve2()}")
+    print(f"Answer 2 (with toposort): {solver.toposort()}")
